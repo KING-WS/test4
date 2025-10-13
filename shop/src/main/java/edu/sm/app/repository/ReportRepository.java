81849e636e852fd53571f74fd49c4937751fb4ca
DESKTOP-T5O03VS\rladn <rladntjd1193@gmail.com>
2025-10-13 14:31:08 +0900

    1013 report 기능 shop 모듈에 추가 정상적으로 작동 확인

---
+++ shop/src/main/java/edu/sm/app/repository/ReportRepository.java
@@ -0,0 +1,11 @@
+package edu.sm.app.repository;
+
+import edu.sm.app.dto.Report;
+import edu.sm.common.frame.SmRepository;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface ReportRepository extends SmRepository<Report, Integer> {
+}
